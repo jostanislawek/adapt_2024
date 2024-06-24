7935fcc09416f4a4ee75b9bfa8e3491aaed270b0
jostanislawek <127526573+jostanislawek@users.noreply.github.com>
2024-06-24 13:31:33 +0100

    Add files via upload

---
+++ paths.py
@@ -0,0 +1,14 @@
+# Full dataset
+data_full = "D:/PhD/Images/Adaptation_Dataset/"
+
+# Sample dataset
+data_train_sample = "D:/PhD/Images/Adaptation_Dataset_Sample"
+data_train_sample_train = "D:/PhD/Images/Adaptation_Dataset_Sample/Train"
+
+model_path = "D:/PhD/Models/Adaptation_2024/model/"
+
+# Vectors
+sample_vectors = "D:/PhD/Images/Vectors/Sample"
+
+vector_path_train = 'D:/PhD/Images/Vectors/Training'
+vector_path_valid = 'D:/PhD/Images/Vectors/Validation'
